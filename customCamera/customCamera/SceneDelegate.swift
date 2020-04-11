6ea5cdd8a12c6239438a44d3071411f396ccec04
Çağrı Tuğberk Masat <tugberkmasat@yahoo.com>
2020-04-12 01:44:42 +0300

    CustomCamera/ Storyboard, captureButton added.

---
+++ customCamera/customCamera/SceneDelegate.swift
@@ -0,0 +1,34 @@
+//
+//  SceneDelegate.swift
+//  customCamera
+//
+//  Created by Çağrı Tuğberk Masat on 11.04.2020.
+//  Copyright © 2020 Çağrı Tuğberk Masat. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+}
---
+++ customCamera/customCamera/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  customCamera
+//
+//  Created by Çağrı Tuğberk Masat on 11.04.2020.
+//  Copyright © 2020 Çağrı Tuğberk Masat. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var cameraView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func imageCaptureButtonPressed(_ sender: Any) {
+    }
+    
+
+}
+
