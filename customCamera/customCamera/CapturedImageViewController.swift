64c41eeef805c3ee766751b6408e4c3000c9b917
Çağrı Tuğberk Masat <tugberkmasat@yahoo.com>
2020-04-12 02:53:40 +0300

    CapturedImageVC, navVC

---
+++ customCamera/customCamera/CapturedImageViewController.swift
@@ -0,0 +1,24 @@
+//
+//  CapturedImageViewController.swift
+//  customCamera
+//
+//  Created by Çağrı Tuğberk Masat on 12.04.2020.
+//  Copyright © 2020 Çağrı Tuğberk Masat. All rights reserved.
+//
+
+import UIKit
+
+class CapturedImageViewController: UIViewController {
+    
+  //  var capturedVideoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var testCapturedImage: UIImage!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    
+
+   
+}
--- customCamera/customCamera/ViewController.swift
+++ customCamera/customCamera/ViewController.swift
@@ -10,38 +10,42 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var cameraView: UIView!
     
+    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var imageCaptureButton: UIButton!
     var captureSession: AVCaptureSession?
     var videoPreviewLayer: AVCaptureVideoPreviewLayer?
     var backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if #available(iOS 10.2, *){
-            let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
-            do {
-                let input = try AVCaptureDeviceInput(device: captureDevice!)
-                captureSession = AVCaptureSession()
-                captureSession?.addInput(input)
-                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
-                videoPreviewLayer?.frame = view.layer.bounds
-                cameraView.layer.addSublayer(videoPreviewLayer!)
-                captureSession?.startRunning()
-            } catch {
-                print("Version Error")
-            }
-            
-        }
+        guard #available(iOS 10.2, *) else {exit(0)}
+        prepareInputSession()
     }
-    
+    override func viewDidAppear(_ animated: Bool) {
+        imageCaptureButton.isHidden = false
+        prepareInputSession()
+    }
     
     @IBAction func imageCaptureButtonPressed(_ sender: Any) {
+        imageCaptureButton.isHidden = true
+        captureSession?.stopRunning()
+        let vc = storyboard?.instantiateViewController(withIdentifier: "CapturedImageViewController") as! CapturedImageViewController
+        navigationController?.pushViewController(vc, animated: true)
+        
     }
-    
-    
-    
-
+    func prepareInputSession() {
+        let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice!)
+            captureSession = AVCaptureSession()
+            captureSession?.addInput(input)
+            videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+            videoPreviewLayer?.frame = view.layer.bounds
+            cameraView.layer.addSublayer(videoPreviewLayer!)
+            captureSession?.startRunning()
+        } catch {
+            print("Version Error")
+        }
+    }
 }
-
