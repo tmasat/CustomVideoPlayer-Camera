0a0b216afb235e15c9daf3184e6fd55924a27a28
Çağrı Tuğberk Masat <tugberkmasat@yahoo.com>
2020-04-12 02:01:01 +0300

    CustomCamera/ captureSession added.

--- customCamera/customCamera/ViewController.swift
+++ customCamera/customCamera/ViewController.swift
@@ -13,13 +13,34 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var cameraView: UIView!
     
+    var captureSession: AVCaptureSession?
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer?
+    var backCamera = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if #available(iOS 10.2, *){
+            let captureDevice = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+            do {
+                let input = try AVCaptureDeviceInput(device: captureDevice!)
+                captureSession = AVCaptureSession()
+                captureSession?.addInput(input)
+                videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession!)
+                videoPreviewLayer?.frame = view.layer.bounds
+                cameraView.layer.addSublayer(videoPreviewLayer!)
+                captureSession?.startRunning()
+            } catch {
+                print("Version Error")
+            }
+            
+        }
     }
     
     
     @IBAction func imageCaptureButtonPressed(_ sender: Any) {
     }
+    
+    
     
 
 }
