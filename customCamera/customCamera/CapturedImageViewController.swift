a5b9d3dfc34544f7bc4a534870d11ad1da084741
Berkay Vurkan <berkayvurkan@yahoo.com>
2020-04-23 15:51:20 +0300

    some UI staffs and improvements

--- customCamera/customCamera/CapturedImageViewController.swift
+++ customCamera/customCamera/CapturedImageViewController.swift
@@ -10,12 +10,38 @@ import UIKit
 import AVFoundation
 
 class CapturedImageViewController: UIViewController {
+    
+    //MARK:- Outlets
+    @IBOutlet weak var capturedPhotoImageView: UIImageView!
+    
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var saveToGalleryButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    
+    
+    //MARK:- Properties
     var takenPhoto: UIImage?
     
-    @IBOutlet weak var capturedPhoto: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        capturedPhoto.image = takenPhoto
+        
+        capturedPhotoImageView.image = takenPhoto
     }
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        if sender == backButton {
+            navigationController?.popViewController(animated: true)
+        } else if sender == saveToGalleryButton {
+            //TODO: Do save action
+            
+        } else if sender == shareButton {
+            //TODO: Do share action
+            
+        }
+    }
+    
 }
